7d3150c7032591198807490638f15282793560f0
Felix <984082450@qq.com>
2018-11-17 15:54:43 +0800

    Log module.

--- HazelByFelix/src/Hazel.h
+++ HazelByFelix/src/Hazel.h
@@ -4,6 +4,7 @@
 
 //For use by Hazel applications
 #include "Hazel/Application.h"
+#include "Hazel/Logger.h"
 
 //----------Entry Point----------
 #include "Hazel/EntryPoint.h"
--- HazelByFelix/src/Hazel/EntryPoint.h
+++ HazelByFelix/src/Hazel/EntryPoint.h
@@ -6,6 +6,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Logger::Init();
+	int temp = 666;
+	LOG_CORE_WARN("Engine Ready");
+	LOG_INFO("hello, Var={0}", temp);
+
 	Hazel::Application* app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
