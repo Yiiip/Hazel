dd9652312b9f67173ea5c7e3d2b59c22410b5d60
Felix <984082450@qq.com>
2018-11-21 00:44:12 +0800

    Fixed submodule path.

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -20,15 +20,15 @@ namespace Hazel {
 }
 
 //For engine core log macros
-#define LOG_CORE_TRACE(...) Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define LOG_CORE_INFO(...)  Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define LOG_CORE_WARN(...)  Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define LOG_CORE_ERROR(...) Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define LOG_CORE_FATAL(...) Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define LOG_CORE_TRACE(...) ::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define LOG_CORE_INFO(...)  ::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define LOG_CORE_WARN(...)  ::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define LOG_CORE_ERROR(...) ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define LOG_CORE_FATAL(...) ::Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 //For client log macros
-#define LOG_TRACE(...) Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define LOG_INFO(...)  Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
-#define LOG_WARN(...)  Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...) Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
-#define LOG_FATAL(...) Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define LOG_TRACE(...) ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define LOG_INFO(...)  ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...)  ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...) ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define LOG_FATAL(...) ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
